b8b0d6949abe944d998aec250198bbbbbf5c34b4
BidulgiCD <31906555+MikumiX@users.noreply.github.com>
2020-07-11 01:24:54 +0900

    큐 풀이

--- Algorithm_Basic_1_BOJ/DataStructure_1/10845.cpp
+++ Algorithm_Basic_1_BOJ/DataStructure_1/10845.cpp
@@ -4,6 +4,8 @@ https://www.acmicpc.net/problem/10845
 */
 
 #include <iostream>
+using namespace std;
+
 //FIFO
 struct MyQueue{
     int data[10000];
@@ -14,13 +16,45 @@ struct MyQueue{
     }
     void push(int num){
         data[end] = num;
-        end += 1;
+        end += 1;        
+    }
+    void pop(){
+        if(empty() == 1){
+            cout << -1 << '\n';              
+        }else{
+            cout << data[begin] << '\n';
+            begin += 1;
+        }
+    }
+    int size(){
+        return end - begin;
+    }
+    int empty(){
+        if(size() == 0)
+            return 1;
+        else
+            return 0;        
+    }
+    void front(){
+        if(empty() == 1){
+            cout << -1 << '\n';
+        }
+        else{
+            cout << data[begin] << '\n';
+        }
+    }
+    void back(){
+        if(empty() == 1){
+            cout << -1 << '\n';               
+        }
+        else{
+            cout << data[end-1] << '\n';
+        }
     }
 };
 
 int main()
 {
-    using namespace std;
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
@@ -34,17 +68,21 @@ int main()
         if(cmd == "push"){
             int n;
             cin >> n;
+            q.push(n);
         } else if(cmd == "pop"){
             // 맨앞의수를 보여주고, 뺀다
+            q.pop();
         } else if(cmd == "size"){
-
+            cout << q.size() << '\n';
         } else if(cmd == "empty"){
-
+            cout << q.empty() << '\n';
         } else if(cmd == "front"){
+            q.front();
             // 없으면 -1 출력
         } else if(cmd == "back"){
+            q.back();
             // 없으면 -1
-        }
+        }        
     }
 
     return 0;

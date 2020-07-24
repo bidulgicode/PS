fe09959cd6b1820ae2191718c1e09a01d807a291
bidulgicode <pkmon1999@gmail.com>
2020-07-24 18:30:20 +0900

    일단 마무리

--- Algorithm_Basic_1_BOJ/DataStructure_1_P/11656.cpp
+++ Algorithm_Basic_1_BOJ/DataStructure_1_P/11656.cpp
@@ -21,11 +21,13 @@ int main()
     sort(a.begin(), a.end(), [&s](int u, int v) {
         // strcmp (https://blockdmask.tistory.com/391)
         // s.c_str() + u, s.c_str() + v
-        // 위 두개의 문자열을 비교하다가 다른문자가 나오는 시점에서
-        // 그 문자의 아스키 코드 값을 비교해서 반환값이 정해짐
+        // 위 두개의 문자열을 비교하다가 서로 다른문자가 나오는 시점에서
+        // 그 문자의 아스키 코드 값을 비교, 반환값이 정해짐
+        // ex) hello vs ello, hello vs llo, ello vs llo 등등..
         // s.c_str() + u < s.c_str() + v 이면 -1
         // s.c_str() + u > s.c_str() + v 이면 1
         // 마지막으로 동일한 경우에는 0
+       
         // 각 문자를 아스키 코드로 비교하기 때문에
         // 1. 대소문자 구분, 2. 각 문자별 숫자로 대소 비교 가능
         // EX) D, F, A의 경우 D < F 이므로 -1, D > A 이므로 1 반환
@@ -33,7 +35,12 @@ int main()
 
         // u, v는 백터 a에 포함된 원소들
         // 이 원소들의 우선순위를 문자열 s와 조합하여 만들기 때문에
+        // 스트링에 + u or v 했을때 여기 해당하는 문자의 아스키값의
+        // 크기를 비교해서 sort 하는거임
         // 2 0 1 3 4 5 이런식의 sort 결과가 나옴
+
+        // u,v 출력하면 모든 경우의 수를 따지지는 않고 있음
+        // 대략 O(logn)?
         cout << u << ',' << v << '\n';
         cout << s.c_str() + u << ',' << s.c_str() + v << '\n';
         return strcmp(s.c_str() + u, s.c_str() + v) < 0;
